1f3896f822a48a0183a34e9bf6df93f2c52a8d67
weskern64 <prins.acosta@gmail.com>
2017-05-19 16:21:34 -0500

    Inicio

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author fservin
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Formato6 marco = new Formato6();
+        
+        marco.setVisible(true);
+   
+    }
+}
